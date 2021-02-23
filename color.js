be5e874c761faa0824b90f3637760acfd502f949
nam <npnam0706@gmail.com>
2021-02-23 23:57:01 +0700

    Add css

---
+++ color.js
@@ -0,0 +1,23 @@
+COLOR_CLASSES = [
+    'text-primary border-primary',
+    'text-secondary border-secondary',
+    'text-danger border-danger',
+    'text-danger border-danger',
+    'text-info border-info',
+    'text-dark border-dark'
+]
+
+COLOR_DATA = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+]
